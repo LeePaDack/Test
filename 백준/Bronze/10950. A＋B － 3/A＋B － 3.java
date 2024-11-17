f1a3012be7fbb105540049fae2433af95dabe2a2
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-11-17 22:12:03 +0900

    [Bronze V] Title: A+B - 3, Time: 188 ms, Memory: 17972 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,16 +1,15 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        
-        for (int i = 0; i < num; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println(a + b);
-        }
-        
-        sc.close();
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 0; i<n;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
 }
